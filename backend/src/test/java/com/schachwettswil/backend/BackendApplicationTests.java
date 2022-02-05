a0d9aff7405b0dfb35a1cbb2b183ab852d754a86
Xynt <xabilro@gmail.com>
2022-02-06 00:07:41 +0100

    rename backend

--- backend/src/test/java/com/schachwettswil/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/schachwettswil/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.schachwettswil.website;
+package com.schachwettswil.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WebsiteApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
