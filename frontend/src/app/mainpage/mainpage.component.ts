8210952b15cda7a01cdb0ec54e835416fa257076
MarlonLoretz <moltgott@gmail.com>
2022-02-11 19:20:31 +0100

    Bi forced mich das commite

--- frontend/src/app/mainpage/mainpage.component.ts
+++ frontend/src/app/mainpage/mainpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../models/article';
 
 @Component({
   selector: 'app-mainpage',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mainpage.component.scss']
 })
 export class MainpageComponent implements OnInit {
+
+  articles: Article[] = [new Article("Artikel 1", "Lorem Ipsum"), new Article("Artikel 2", "Ülemaa >")];
 
   constructor() { }
 
---
+++ frontend/src/app/models/article.ts
@@ -0,0 +1,13 @@
+export class Article {
+
+public text: string = "";
+public title: string = "";
+
+constructor(title: string, text: string) {
+  this.text = text;
+  this.title= title;
+}
+
+
+
+}
